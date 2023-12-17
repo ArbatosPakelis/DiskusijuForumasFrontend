fef6bdf944852ac5438ebf9d8c8a1b89f8703edf
ArbatosPakelis <tomasjuodelis123@gmail.com>
2023-12-17 04:09:39 +0200

    deployment 15

--- src/apis/defaultApi.js
+++ src/apis/defaultApi.js
@@ -2,11 +2,11 @@ import axios from "axios";
 const BASE_URL = "https://diskusiju-forumas-89f8e927e19e.herokuapp.com"
 
 export default axios.create({
-    baseUrl: BASE_URL
+    baseURL: BASE_URL
 });
 
 export const PrivateApi = axios.create({
-    baseUrl: BASE_URL,
+    baseURL: BASE_URL,
     headers: {'Content-Type': 'application/json'},
     withCredentials: true
 });
